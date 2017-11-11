bfbe3afaf953a5377c3fe8cd57730dc980cddb9a
GluGnure <hlib.hluhovskyi@nure.ua>
2017-11-11 14:22:09 +0200

    Swing GUI and MainFrame Tests added

---
+++ usermanagement/src/test/java/usermanagement/db/MockUserDao.java
@@ -0,0 +1,52 @@
+package usermanagement.db;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import usermanagement.User;
+
+public class MockUserDao implements UserDao {
+	private long id = 0;
+	private Map<Long, User> users = new HashMap<> ();
+	
+	public User create(User user) throws DatabaseException {
+        Long currentId = new Long(++id);
+        user.setId(currentId);
+        users.put(currentId, user);
+        return user;
+    }
+
+    public void update(User user) throws DatabaseException {
+        Long currentId = user.getId();       
+        users.remove(currentId);
+        users.put(currentId, user);
+    }
+
+    public void delete(User user) throws DatabaseException {
+        Long currentId = user.getId();
+        users.remove(currentId);
+    }
+
+    public User find(Long id) throws DatabaseException {
+        
+        return (User) users.get(id);
+    }
+
+    public Collection<User> findAll() throws DatabaseException {
+        return users.values();
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * 
+     */
+    public Collection<User> find(String firstName, String lastName) throws DatabaseException {
+        throw new UnsupportedOperationException();
+    }
+
+}
